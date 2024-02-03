33c14a43a14344dc96370c526abee5c4c93151ef
=mekadmini <medislamm123@TUF-GAMING>
2024-02-03 15:57:00 +0100

    update coverter

--- src/parkmylatex/converter.py
+++ src/parkmylatex/converter.py
@@ -1,1 +1,23 @@
-import numpy as np
+from openai import OpenAI
+from typing import Optional
+
+client = OpenAI()
+
+def rephrase(text:str, modification_degree:Optional[str] = None)-> str:
+
+    match modification_degree:
+        case "correct-only":
+            prompt = ""
+        case "correct-with-minor-changes":
+            prompt = ""
+        case _:
+            prompt = ""
+
+    completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", "content": prompt + "\n" + text}
+    ]
+    )
+
+    print(completion.choices[0].message)
