e57fdc5b1269d0cebe8a665bff06430807685ed2
gehasel <mail@haselbeck.org>
2024-02-03 16:27:15 +0100

    read api key from .env

--- src/parkmylatex/converter.py
+++ src/parkmylatex/converter.py
@@ -1,10 +1,17 @@
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
 from typing import Optional
+from pathlib import Path
 
-client = OpenAI()
+package_root = Path(__file__).parent.parent.parent
+env_path = package_root / '.env'
+load_dotenv(dotenv_path=env_path)
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
 
-def rephrase(text:str, modification_degree:Optional[str] = None)-> str:
 
+def rephrase(text: str, modification_degree: Optional[str] = None) -> str:
     match modification_degree:
         case "correct-only":
             prompt = ""
@@ -14,10 +21,8 @@ def rephrase(text:str, modification_degree:Optional[str] = None)-> str:
             prompt = ""
 
     completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "user", "content": prompt + "\n" + text}
-    ]
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": prompt + "\n" + text}],
     )
 
     print(completion.choices[0].message)
